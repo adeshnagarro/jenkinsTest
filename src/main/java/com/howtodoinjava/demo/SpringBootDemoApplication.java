0f96fe69a81967de1c02351dfecf376cca57c183
adeshnagarro <83108483+adeshnagarro@users.noreply.github.com>
2021-04-24 19:06:19 +0530

    change

--- src/main/java/com/howtodoinjava/demo/SpringBootDemoApplication.java
+++ src/main/java/com/howtodoinjava/demo/SpringBootDemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
-	System.out.println("***Running***");
-		SpringApplication.run(SpringBootDemoApplication.class, args);
+	SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
 }
